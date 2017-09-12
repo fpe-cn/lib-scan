b2676ce20cb037b16caeb3e044939d7ed39f9f7b
Renaud Dierickx <renaud.dierickx@compte-nickel.fr>
2017-09-12 15:55:08 +0200

    Ajout des ressources du scanner

---
+++ myapp.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./res/");
+
+var server = http.createServer(function(req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+server.listen(process.env.PORT, '0.0.0.0');
